866fd1830bdfa73786430ee094da75ccb8c07077
Swapnil Jain <jainswapnil138@gmail.com>
2024-07-12 09:51:12 +0530

    Initial commit

---
+++ python/src/auth/server.py
@@ -0,0 +1,50 @@
+import jwt, datetime, os #for auth
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server= Flask(__name__)
+
+mysql=MySQL(server) #instance of MySQL
+
+#config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth=request.authorization  #auth should have authenticated header.
+    if not auth:
+        return("missing credentials", 401) #in case doesnt have authorization header
+    
+    #check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+    )
+
+    if res > 0: #means result exists in database
+        user_row = cur.fetchone() #gives a tuple 
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username !=email or auth.password !=password:
+            return("invalid credentials", 401)
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+    else:
+        return("invalid credentials", 401)
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.istnow() + datetime.timedelta(days=1), #token expires in 24 hours
+            "iat": datetime.datetime.istnow(),  #when token was issued.
+            "admin": authz, #whether user is admin or not
+        },
+        secret,
+        algorithm="HS256",
+    )
