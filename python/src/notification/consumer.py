7acfcc47b81a92dca71eb5c2c161af3e7fb80f74
Swapnil Jain <jainswapnil138@gmail.com>
2024-07-15 11:13:25 +0530

    Added notification functionality. Added docker ignore files.

---
+++ python/src/notification/consumer.py
@@ -0,0 +1,42 @@
+import pika
+import sys
+import os
+from send import email
+
+
+def main():
+    #rabbitmq
+    connection= pika.BlockingConnection(pika.ConnectionParameters(
+        host="rabbitmq-service",
+        port=5673,  
+    ))
+    channel = connection.channel()
+
+    def callback(chan, method, properties, body):
+        err = email.notify(body)
+        if err:
+            chan.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            chan.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(
+        queue=os.environ.get("MP3_QUEUE"),
+        on_message_callback=callback
+    )
+
+    print("Waiting for messages. To leave: CTRL+C")
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted. Exiting..")
+        # Graceful exit
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
+#Note no port is exposed. This is cuz this is a consumer, and not a service that we are making requests to.
---
+++ python/src/notification/send/email.py
@@ -0,0 +1,32 @@
+import smtplib
+import os
+import json
+
+from email.message import EmailMessage
+
+def notify(message):
+    try:
+        message = json.loads(message)  #takes a file object and returns the json object
+        mp3_fid = message.get("mp3_fid")
+        sender_address = os.environ.get("GMAIL_USER")
+        sender_pass = os.environ.get("GMAIL_PASS")
+        recipient_address = message.get("username")
+
+        # Creating message
+        msg = EmailMessage()
+        msg.set_content(
+            f"Your MP3 file is ready for download.\nMP3 file_id: {mp3_fid}")
+        msg["Subject"] = "MP3 Conversion Complete"
+        msg["From"] = sender_address
+        msg["To"] = recipient_address
+
+        session = smtplib.SMTP("smtp.gmail.com", 587)
+        session.starttls()  #ensures security b/w our app and smtp server.
+        session.login(sender_address, sender_pass)
+        session.send_message(msg, sender_address, recipient_address)
+        session.quit()
+        print("Email sent successfully")
+
+    except Exception as e:
+        print(e)
+        return e
